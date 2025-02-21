bf70a3753ddfbafb3148614d4dd09d9af4f976eb
efecarranza <fcarr004@fiu.edu>
2025-02-21 13:35:54 -0300

    feat: scaffold bridge

--- src/common/CcipBridge.sol
+++ src/common/CcipBridge.sol
@@ -2,9 +2,15 @@
 
 pragma solidity 0.8.27;
 
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "openzeppelin/access/Ownable.sol";
 
-contract CcipBridge is Ownable {
+import {ICcipBridge} from "src/common/ICcipBridge.sol";
+
+contract CcipBridge is ICcipBridge, Ownable {
+    using SafeERC20 for IERC20;
+
     constructor(address initialOwner) Ownable(initialOwner) {}
 
     function bridge() external {}
